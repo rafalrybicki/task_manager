e27014b15be39657e729aca9b0d60856fd83d5ca
Rafal Rybicki <rafarybicki@gmail.com>
2020-10-04 11:33:42 +0200

    Add AppBar Component

---
+++ src/components/AppBar.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+      this is AppBar
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+   div {
+      height: 43px;
+      padding: 5px 12px;
+      background-color: #FAFAFA;
+      position: sticky;
+   }
+</style>
