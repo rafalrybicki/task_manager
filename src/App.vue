9ae60f13422063b4d3e231dda1c6963be6c653f2
Rafal Rybicki <rafarybicki@gmail.com>
2020-10-04 11:44:40 +0200

    Add Menu component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
 	<div id="app">
 		<AppBar />
+		<Menu />
 	</div>
 </template>
 
 <script>
 import AppBar from './components/AppBar.vue'
+import Menu from './components/Menu.vue'
 
 export default {
 	name: 'App',
 	components: {
-		AppBar
+		AppBar,
+		Menu
 	}
 }
 </script>
 
 <style>
-
+	#app {
+		height: 100%;
+		width: 100%;
+	}
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,30 @@
+<template>
+   <nav class="h100">
+      <ul>
+         
+      </ul>
+   </nav>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         items: [
+            {to: '/asap', title: 'ASAP', icon: '?'},
+            {to: '/inbox', title: 'Inbox', icon: '?'},
+            {to: '/today', title: 'Today', icon: '?'},
+            {to: '/upcoming', title: 'Upcoming', icon: '?'},
+         ]
+      }
+   }
+}
+</script>
+
+<style scoped>
+   nav {
+      background-color: #FAFAFA;
+      width: 260px;
+      padding: 12px;
+   }
+</style>
